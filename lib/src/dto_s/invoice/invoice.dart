eebc78ff00f67e5d1b291ef16aa6594371321d1e
Amir <amirsol456@gmail.com>
2026-02-13 02:35:35 +0330

    files

--- lib/src/dto_s/invoice/invoice.dart
+++ lib/src/dto_s/invoice/invoice.dart
@@ -2,14 +2,21 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'invoice.g.dart';
 
+
+
+
 @JsonSerializable()
 class Invoice {
-  final String id;
-  final String? invoiceNo;
-  final String? type;
-  final String? partyId;
+  final int id;  // تغییر از String به int
+  final String invoiceNo;
+  @JsonKey(defaultValue: 'Sale')  // مقدار پیش‌فرض
+  final String type;
+  final int? partyId;
+  final int? sellerEmployeeId;
+  final DateTime date;
   final double totalAmount;
   final String status;
+  final String? notes;
   final int version;
   final bool isDeleted;
   final DateTime createdAt;
@@ -19,19 +26,51 @@ class Invoice {
     required this.id,
     required this.invoiceNo,
     required this.type,
-    required this.partyId,
+    this.partyId,
+    this.sellerEmployeeId,
+    required this.date,
     required this.totalAmount,
     required this.status,
+    this.notes,
     required this.version,
     required this.isDeleted,
     required this.createdAt,
     required this.updatedAt,
   });
-  
 
-
-  factory Invoice.fromJson(Map<String, dynamic> json) =>
-      _$InvoiceFromJson(json);
-
+  factory Invoice.fromJson(Map<String, dynamic> json) => _$InvoiceFromJson(json);
   Map<String, dynamic> toJson() => _$InvoiceToJson(this);
 }
+// @JsonSerializable()
+// class Invoice {
+//   final String id;
+//   final String? invoiceNo;
+//   final String? type;
+//   final String? partyId;
+//   final double totalAmount;
+//   final String status;
+//   final int version;
+//   final bool isDeleted;
+//   final DateTime createdAt;
+//   final DateTime updatedAt;
+//
+//   Invoice({
+//     required this.id,
+//     required this.invoiceNo,
+//     required this.type,
+//     required this.partyId,
+//     required this.totalAmount,
+//     required this.status,
+//     required this.version,
+//     required this.isDeleted,
+//     required this.createdAt,
+//     required this.updatedAt,
+//   });
+//
+//
+//
+//   factory Invoice.fromJson(Map<String, dynamic> json) =>
+//       _$InvoiceFromJson(json);
+//
+//   Map<String, dynamic> toJson() => _$InvoiceToJson(this);
+// }
