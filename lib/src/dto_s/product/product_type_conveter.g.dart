afef672857c16695963112115c68299eba5b9f97
amirsol456456 <amirsol456@gmail.com>
2026-02-27 11:25:30 +0330

    ok

--- lib/src/dto_s/product/product_type_conveter.g.dart
+++ lib/src/dto_s/product/product_type_conveter.g.dart
@@ -6,6 +6,8 @@ part of 'product_type_conveter.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+AnimalTypeConverter _$AnimalTypeConverterFromJson(Map<String, dynamic> json) =>
+    AnimalTypeConverter();
 
 Map<String, dynamic> _$AnimalTypeConverterToJson(
   AnimalTypeConverter instance,
